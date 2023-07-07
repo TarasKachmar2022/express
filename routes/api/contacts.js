a84b891f43dd9e6d6d09d53b65befb73df27eff7
Taras Kachmar <kachmar.taras@gmail.com>
2023-07-07 04:10:38 +0300

    done express vanilla

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,64 @@
 const express = require("express");
 
+const Joi = require("joi");
+
+const contacts = require("../../models/contacts");
+
+const { HttpError } = require("../../utils");
+
 const router = express.Router();
 
-const ctrl = require("../../controllers/contacts");
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
 
-router.get("/", ctrl.getContacts);
+router.get("/", async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+});
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+});
 
-router.post("/", ctrl.addContact);
+router.post("/", async (req, res) => {
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+});
 
-router.delete("/:contactId", ctrl.removeContact);
+router.delete("/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Contact deleted",
+  });
+});
 
-router.put("/:contactId", ctrl.updateContact);
+router.put("/:contactId", async (req, res) => {
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+});
 
 module.exports = router;
